df9cf424ee3b5c787ddaa0204602294f60a66f6f
caiovilquer <caiovcarvalho337@gmail.com>
2025-05-17 16:07:26 -0300

    fix: correct base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<portifolio>/',              
+  base: '/portifolio/',              
   plugins: [react()],
   server: { port: 5173 }
 });
