8c64abc94c819130a1e6e6b179808a32f59c6d86
caiovilquer <caiovcarvalho337@gmail.com>
2025-05-17 15:07:01 -0300

    chore: update dependencies and configure Vite for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<NOME_DO_REPO>/',              // substituir <NOME_DO_REPO>
+  base: '/<portifolio>/',              
   plugins: [react()],
   server: { port: 5173 }
 });
