b81d8097a225d8ae016e681e88016615e043e453
caiovilquer <caiovcarvalho337@gmail.com>
2025-05-17 16:55:45 -0300

    feat: wrap application in ThemeProvider and add section classes for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,23 +7,23 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 const App: React.FC = () => (
-  <>
+  <ThemeProvider>
     <Navbar />
     <main className="mt-16">
-      <section id="home">
+      <section id="home" className="section">
         <Home />
       </section>
-      <section id="about">
+      <section id="about" className="section">
         <About />
       </section>
-      <section id="projects">
+      <section id="projects" className="section">
         <Projects />
       </section>
-      <section id="contact">
+      <section id="contact" className="section">
         <Contact />
       </section>
     </main>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
